d0057b45e4c84a9213df7c8f7aaa2045b350f364
Barath s <112713529+freqcoder19@users.noreply.github.com>
2025-04-17 15:48:59 +0530

    Add files via upload

--- frontend/src/components/components_lite/AppliedJob.jsx
+++ frontend/src/components/components_lite/AppliedJob.jsx
@@ -33,7 +33,7 @@ const AppliedJob = () => {
               <TableRow key={appliedJob._id}>
                 <TableCell>{appliedJob?.createdAt.split("T")[0]}</TableCell>
                 <TableCell>{appliedJob.job?.title}</TableCell>
-                <TableCell>{appliedJob.job?.company.name}</TableCell>
+                <TableCell>{appliedJob.job?.company?.name || "N/A"}</TableCell>
                 <TableCell className="text-right">
                   <Badge
                     className={`${
