35d56a1e229a6d26bd3c3431a82a520fc88000a7
Barath s <112713529+freqcoder19@users.noreply.github.com>
2025-06-15 11:30:01 +0530

    Add files via upload

--- frontend/src/components/components_lite/Header.jsx
+++ frontend/src/components/components_lite/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
     navigate("/browse");
   };
   return (
-    <div>
+    <div className="bg-[#dbf3dc] min-h-screen">
       <div className="text-center">
         <div className="flex flex-col gap-5 my-10">
           <span className="px-4 mx-auto flex justify-center items-center py-2 gap-2 rounded-full bg-gray-200 text-red-600 font-medium">
@@ -24,17 +24,20 @@ const Header = () => {
               {" "}
               <PiBuildingOfficeBold />
             </span>{" "}
-            No.1 Job Hunt Website
+            Top startup job Website
           </span>
 
-          <h2 className="text-5xl font-bold">
-            Search Apply & <br />
-            Get Your <span className="text-[#6A38C2]">Dream Job</span>
-          </h2>
+          <h2 className="text-5xl font-bold text-#f4f4f4">
+  Search Apply & <br />
+  Get Your{" "}
+  
+    Career Started
+
+</h2>
+
           <p>
-            Start your hunt for the best, life-changing career opportunities
-            from here in your <br />
-            selected areas conveniently and get hired quickly.
+            Start your search for the best, life-changing career opportunities
+            in here <br />
           </p>
           <div className="flex w-[40%] shadow-lg border border-gray-300 pl-3 rounded-full  items-center gap-4 mx-auto ">
             <input
